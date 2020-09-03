0c520bf7840e366aa8a80e8639b6c426b86290ca
Lena <lena.osterhus@gmail.com>
2020-09-03 15:25:07 +0200

    move as a function

--- assignment2/move.sh
+++ assignment2/move.sh
@@ -1,39 +1,41 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# Checking that two arguments were provided
-if [ $# -lt 2 ]; then
-	echo "Usage: Source & destination. Option: file type (*.txt *.dat ...)"
-	exit
-fi
 
-function checkDirectory {
-    # Checks if argument is a directory
+function move {
 
-	if ! [ -d $1 ]; then
-        echo "$1 is not a directory"
-        exit
-    fi
+	# Checking that at least two arguments were provided
+	if [ $# -lt 2 ]; then
+		echo "Usage: source destination [file type (*.txt *.dat ...)]"
+		return
+	fi
+
+
+	src=$1
+	dst=$2
+
+	# Checking that arguments are directories
+	if ! [ -d $src ]; then
+		echo "$src is not a directory"
+		return
+	fi
+	if ! [ -d $dst ]; then
+		echo "$dst is not a directory"
+		return
+	fi
+
+	# Files that are to be moved
+	# 2.1a
+	if [ $# == 3 ]; then
+	    files=$(ls "${src}/$3")
+	    # Bash will print "No such file or directory" if no files with file extention
+	else
+	    files=$(ls $src)
+	fi
+
+	# Moving files
+	for file in $files
+	    do
+	        echo "Moving $file"
+	        mv $src"/"$file $dst
+	    done
 }
-
-src=$1
-dst=$2
-
-# Checking that arguments are directories
-checkDirectory $src
-checkDirectory $dst
-
-# Files that are to be moved
-# 2.1a
-if [ $# == 3 ]; then
-    files=$(ls "${src}/$3")
-    # Bash will print "No such file or directory" if no files with file extention
-else
-    files=$(ls $src)
-fi
-
-# Moving files
-for file in $files
-do
-    echo "Moving $file"
-    mv $src"/"$file $dst
-done
