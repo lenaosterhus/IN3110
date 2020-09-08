1d3bb70f6db30fb8f3d5384dd1fd8003ae18e5c0
Lena <lena.osterhus@gmail.com>
2020-09-08 11:57:33 +0200

    Task 3.1 done

--- assignment3/wc.py
+++ assignment3/wc.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
+"""A Python implementation which counts the lines, words and character of files.
+
+When called with a file name as command line argument, the implementation prints
+the single line a b c fn where a is the number of lines in the file, b the
+number of words, c the number of characters, and fn the filename.
+
+A line is defined as a string of characters delimited by a <newline> character.
+A word is defined as a string of characters delimited by white space characters.
+A character is defined as the smallest unit of information that includes space,
+tab and newline.
+"""
 
 import sys
+import os
 
-file_name = sys.argv[1]
+def word_count():
 
-with open(file_name, 'r') as file:
-    lines = 0
-    """A line is defined as a string of characters delimited
-         by a <newline> character"""
-    words = 0
-    """A word is defined as a string of characters delimited by white space
-         characters."""
-    characters = 0
-    """A character is the smallest unit of information that includes space, tab and newline."""
+    total_lines = 0
+    total_words = 0
+    total_characters = 0
 
-    for line in file:
-        lines += 1
-        words += len(line.split())
-        characters += len(line)
+    for path in sys.argv[1:]:
 
-    print(f"{lines}   {words}   {characters}   {file_name}")
+        if os.path.isfile(path):
+
+            with open(path, 'r') as file:
+                lines = 0
+                words = 0
+                characters = 0
+
+                for line in file:
+                    lines += 1
+                    words += len(line.split())
+                    characters += len(line)
+
+                print(f"{lines:>7} {words:>7} {characters:>7} {path}")
+
+                total_lines += lines
+                total_words += words
+                total_characters += characters
+
+    if len(sys.argv) > 2:
+        print(f"{total_lines:>7} {total_words:>7} {total_characters:>7} total")
+
+word_count()
