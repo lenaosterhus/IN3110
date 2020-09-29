f0d5374060ef77d9e73ed48424f7e927715d7c53
Lena <lena.osterhus@gmail.com>
2020-09-29 15:56:22 +0200

    Filters now with optional implementation and scaling

--- assignment4/instapy/filters.py
+++ assignment4/instapy/filters.py
@@ -1,41 +1,106 @@
 from cv2 import cv2
 import numpy as np
-from instapy.grayscale.numpy_color2gray import numpy_color2gray
-from instapy.sepia.numpy_color2sepia import numpy_color2sepia
+import os
 
-def grayscale_image(input_filename, output_filename=None):
+from instapy.grayscale.numpy_color2gray import numpy_color2gray
+from instapy.grayscale.numba_color2gray import numba_color2gray
+from instapy.grayscale.python_color2gray import python_color2gray
+
+from instapy.sepia.numpy_color2sepia import numpy_color2sepia
+from instapy.sepia.numba_color2sepia import numba_color2sepia
+from instapy.sepia.python_color2sepia import python_color2sepia
+
+def grayscale_image(input_filename, output_filename=None, implementation="numpy", scale=1.0):
     """Converts a color image to grayscale.
 
     Args:
         input_filename (string): the image's filepath.
         output_filename (string, optional): The filepath for where to save the result (inkl. file extension). Defaults to None.
+        implementation (string, optional): The implementation for the conversion. Options are "python", "numba" and "numpy". Defaults to "numpy".
+        scale (float, optional): Value for how much to scale the image. Must be > 0 and <= 1.0. Defaults to 1.0.
+
+    Raises:
+        FileNotFoundError: If the input_filename does not exist.
+        ValueError: If the wrong implementation type or scale is given.
 
     Returns:
         ndarray: 2D array of unsigned integers, representing the grayscale image.
     """
+    cwd = os.getcwd()
+    input_filename = os.path.join(cwd, input_filename)
+    
+    if not os.path.isfile(input_filename):
+        raise FileNotFoundError
     image_array = cv2.imread(input_filename)
 
-    grayscale_image = numpy_color2gray(image_array)
+    if scale == 1.0:  
+        # do nothing
+        pass
+    elif scale < 1 and scale > 0:
+        image_array = cv2.resize(image_array, (0, 0), fx=scale, fy=scale)
+    else:
+        raise ValueError(
+            f"Scaling with {scale} is not possible. Must be > 0 and <= 1.0")
+
+    if implementation == "numpy":
+        grayscale_image = numpy_color2gray(image_array)
+    elif implementation == "numba":
+        grayscale_image = numba_color2gray(image_array)
+    elif implementation == "python":
+        grayscale_image = python_color2gray(image_array)
+    else:
+        raise ValueError(
+            f"Implementation '{implementation}' is not valid. Must be 'python', 'numba' or 'numpy'")
 
     if output_filename != None:
+        output_filename = os.path.join(cwd, output_filename)
         cv2.imwrite(output_filename, grayscale_image)
     return grayscale_image
     
     
-def sepia_image(input_filename, output_filename=None):
+def sepia_image(input_filename, output_filename=None, implementation="numpy", scale=1.0):
     """Converts a color image to sepia.
 
     Args:
         input_filename (string): the image's filepath.
         output_filename (string, optional): The filepath for where to save the result (inkl. file extension). Defaults to None.
+        implementation (string, optional): The implementation for the conversion. Options are "python", "numba" and "numpy". Defaults to "numpy".
+        scale (float, optional): Value for how much to scale the image. Must be > 0 and <= 1.0. Defaults to 1.0.
+
+    Raises:
+        FileNotFoundError: If the input_filename does not exist.
+        ValueError: If the wrong implementation type or scale is given.
 
     Returns:
         ndarray: 3D array of unsigned integers, representing the sepia image.
     """
+    cwd = os.getcwd()
+    input_filename = os.path.join(cwd, input_filename)
+
+    if not os.path.isfile(input_filename):
+        raise FileNotFoundError
     image_array = cv2.imread(input_filename)
 
-    sepia_image = numpy_color2sepia(image_array)
+    if scale == 1.0: 
+        # do nothing
+        pass
+    elif scale < 1 and scale > 0:
+        image_array = cv2.resize(image_array, (0, 0), fx=scale, fy=scale)
+    else:
+        raise ValueError(
+            f"Scaling with {scale} is not possible. Must be > 0 and <= 1.0")
+
+    if implementation == "numpy":
+        sepia_image = numpy_color2sepia(image_array)
+    elif implementation == "numba":
+        sepia_image = numba_color2sepia(image_array)
+    elif implementation == "python":
+        sepia_image = python_color2sepia(image_array)
+    else:
+        raise ValueError(
+            f"Implementation '{implementation}' is not valid. Must be 'python', 'numba' or 'numpy'")
 
     if output_filename != None:
+        output_filename = os.path.join(cwd, output_filename)
         cv2.imwrite(output_filename, sepia_image)
     return sepia_image
