0c48bda2e6bcaac44f0b2f5a8c2a885f8195d6dc
Lena <lena.osterhus@gmail.com>
2020-09-30 13:31:01 +0200

    Update setup.py

--- assignment4/setup.py
+++ assignment4/setup.py
@@ -3,7 +3,10 @@ import setuptools
 setuptools.setup(
     name='instapy',
     version='1.0',
-    author='Lena Osterhus',
+    author='lenamos',
     packages=setuptools.find_packages(),
-    scripts=['bin/instapy']
+    scripts=['bin/instapy'],
+    setup_requires=['numpy', 'setuptools>=18.0'],
+    install_requires=['numpy', 'numba', 'opencv-python'],
+    python_requires='>=3.6'
 )
